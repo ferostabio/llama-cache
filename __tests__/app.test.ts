e72009be83792f32715081461ddf1ebaa7af017b
Federico Erostarbe <erostarbe@gmail.com>
2023-08-22 12:15:06 -0300

    chore: basic tests

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,19 +1,30 @@
 import request from "supertest";
 import app, { server } from "../index.ts";
-import { disconnectFromRedis, getLlamaFinancials } from "../helpers/redis.ts";
+import { disconnectFromRedis, getFinancials } from "../helpers/redis.ts";
+import { fetchFinancialsApi, fetchStakingData } from "../helpers/api.ts";
 
-// Bare minimum
+// Bare minimum, need to test all functions with mocks
 describe("Express App API", () => {
-  it("API request should respond with proper json", async () => {
-    const response = await request(app).get("/llama_financials");
-
+  it("/financials request should execute successfully", async () => {
+    const response = await request(app).get("/financials");
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("lendBorrows");
-    expect(response.body).toHaveProperty("pools");
   });
 
-  it("Redis returns proper json", async () => {
-    const data = await getLlamaFinancials();
+  it("API should fetch financial data", async () => {
+    const result = await fetchFinancialsApi();
+
+    expect(result).toHaveProperty("lendBorrows");
+    expect(result).toHaveProperty("pools");
+  });
+
+  it("API should fetch staking data", async () => {
+    const result = await fetchStakingData();
+
+    expect(result).toHaveLength(1);
+  });
+
+  it("Redis should return proper json", async () => {
+    const data = await getFinancials();
 
     expect(data.lendBorrows).not.toBeUndefined();
     expect(data.pools).not.toBeUndefined();
